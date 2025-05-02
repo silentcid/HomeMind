c957a843b72953f3d246b9b209a755a0dc6cfaa5
Josh Gebbeken <jgebbeken@gmail.com>
2025-05-02 16:48:44 +0900

    changed package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.homemind"
+    namespace = "com.silentcid.homemind"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.homemind"
+        applicationId = "com.silentcid.homemind"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
